9929aa2fcfbc77076b80b23f8670c20ea0bf8e96
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-08 00:24:50 +0530

    Refactor header options in work layout

--- ww-frontend/app/(tabs)/work/_layout.jsx
+++ ww-frontend/app/(tabs)/work/_layout.jsx
@@ -1,15 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+import Header from "../../components/common/Header";
 
 const PayLayout = () => {
+  const headerOptions = Header();
   return (
-    <Stack>
-      <Stack.Screen name='index' options={{headerShown: true, title: 'Work', headerTitleAlign: 'center'}} />
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        headerTitleAlign: "center",
+        headerRight: headerOptions.headerRight,
+        headerShadowVisible: false,
+        headerTitleStyle: {
+          fontSize: 24,
+          fontWeight: "bold",
+          fontFamily: "sans-serif-condensed",
+          color: "#3498DB",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Work",
+        }}
+      />
     </Stack>
-  )
-}
+  );
+};
 
-export default PayLayout
+export default PayLayout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
