9a2392483c2202f0b5dd6f12b61f7ed4e2b46c30
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-08 00:23:15 +0530

    Refactor header options in home layout

--- ww-frontend/app/(tabs)/home/_layout.jsx
+++ ww-frontend/app/(tabs)/home/_layout.jsx
@@ -1,16 +1,30 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
+import Header from "../../components/common/Header";
 
 const _layout = () => {
+  const headerOptions = Header();
+
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        headerTitleAlign: "center",
+        headerRight: headerOptions.headerRight,
+        headerShadowVisible: false,
+        headerTitleStyle: {
+          fontSize: 24,
+          fontWeight: "bold",
+          fontFamily: "sans-serif-condensed",
+          color: "#3498DB",
+        },
+      }}
+    >
       <Stack.Screen
         name="index"
         options={{
-          headerShown: true,
-          title: "Home",
-          headerTitleAlign: "center",
+          title: headerOptions.title,
         }}
       />
     </Stack>
