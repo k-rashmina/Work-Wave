1d922cfe7e4cf9c1e3e1aa1e2118c7bb14e593df
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-07 09:27:23 +0530

    Refactor _layout.jsx to improve tab icon styling and code readability

--- ww-frontend/app/(tabs)/_layout.jsx
+++ ww-frontend/app/(tabs)/_layout.jsx
@@ -1,93 +1,121 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
-import Icons from "react-native-vector-icons/Ionicons"
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import Icons from "react-native-vector-icons/Ionicons";
 
-const TabIcon = ({icon, iconFilled, color, title, focused}) => {
-  return(
+const TabIcon = ({ icon, iconFilled, color, title, focused }) => {
+  return (
     <View style={styles.tabIcon}>
-      {
-        focused ? 
-          <Icons name={iconFilled} size={25} color={color} /> :
-          <Icons name={icon} size={25} color={color} /> 
-      }
-      {
-        focused &&
-       <Text style={{fontWeight: `${focused ? 'bold' : 'normal'}`, color: color}}>{title}</Text>
-      }
+      {focused ? (
+        <Icons name={iconFilled} size={25} color={color} />
+      ) : (
+        <Icons name={icon} size={25} color={color} />
+      )}
+      {focused && (
+        <Text
+          style={{ fontWeight: `${focused ? "bold" : "normal"}`, color: color }}
+        >
+          {title}
+        </Text>
+      )}
     </View>
-  )
-}
+  );
+};
 
 const TabsLayout = () => {
   return (
     <>
-      <Tabs screenOptions={{
-        tabBarShowLabel: false, 
-        tabBarStyle: {
-          height: 84,
-          backgroundColor: '#3498DB',
-          borderTopWidth: 1,
-          borderTopColor: '#EEEEEE'
-        },
-        tabBarActiveTintColor: '#fff',
-        tabBarInactiveTintColor: '#fff',
-        }}>
-        <Tabs.Screen 
-          name="home" 
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            height: 84,
+            backgroundColor: "#3498DB",
+            borderTopWidth: 1,
+            borderTopColor: "#EEEEEE",
+          },
+          tabBarActiveTintColor: "#fff",
+          tabBarInactiveTintColor: "#fff",
+        }}
+      >
+        <Tabs.Screen
+          name="home"
           options={{
             title: "Home",
             headerShown: false,
-            tabBarIcon: ({color, focused}) => (
-              <TabIcon color={color} icon={'home-outline'} iconFilled={'home'} title="Home" focused={focused} />
-            )
-          }} 
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                color={color}
+                icon={"home-outline"}
+                iconFilled={"home"}
+                title="Home"
+                focused={focused}
+              />
+            ),
+          }}
         />
 
-        <Tabs.Screen 
-          name="jobs" 
+        <Tabs.Screen
+          name="jobs"
           options={{
             title: "Jobs",
             headerShown: false,
-            tabBarIcon: ({color, focused}) => (
-              <TabIcon color={color} icon={'briefcase-outline'} iconFilled={'briefcase'} title="Jobs" focused={focused} />
-            )
-          }} 
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                color={color}
+                icon={"briefcase-outline"}
+                iconFilled={"briefcase"}
+                title="Jobs"
+                focused={focused}
+              />
+            ),
+          }}
         />
 
-        <Tabs.Screen 
-          name="work" 
+        <Tabs.Screen
+          name="work"
           options={{
             title: "Work",
             headerShown: false,
-            tabBarIcon: ({color, focused}) => (
-              <TabIcon color={color} icon={'construct-outline'} iconFilled={'construct'} title="Work" focused={focused} />
-            )
-          }} 
-        />       
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                color={color}
+                icon={"construct-outline"}
+                iconFilled={"construct"}
+                title="Work"
+                focused={focused}
+              />
+            ),
+          }}
+        />
 
-        <Tabs.Screen 
-          name="profile" 
+        <Tabs.Screen
+          name="profile"
           options={{
             title: "Profile",
             headerShown: false,
-            tabBarIcon: ({color, focused}) => (
-              <TabIcon color={color} icon={'person-outline'} iconFilled={'person'} title="Profile" focused={focused} />
-            )
-          }} 
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                color={color}
+                icon={"person-outline"}
+                iconFilled={"person"}
+                title="Profile"
+                focused={focused}
+              />
+            ),
+          }}
         />
-
       </Tabs>
     </>
-  )
-}
+  );
+};
 
-export default TabsLayout
+export default TabsLayout;
 
 const styles = StyleSheet.create({
   tabIcon: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     gap: 3,
-  }
-})
+  },
+});
