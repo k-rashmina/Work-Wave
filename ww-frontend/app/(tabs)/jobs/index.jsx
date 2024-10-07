05305d9d3e8ac1602cf7ed45c6d410536f9ae3a7
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-08 00:23:55 +0530

    Refactor styles in jobs layout

--- ww-frontend/app/(tabs)/jobs/index.jsx
+++ ww-frontend/app/(tabs)/jobs/index.jsx
@@ -1,14 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const Dashboard = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Jobs</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+});
