ee2262be11c72738ab6834f0eea7b923e161a6bd
IT22338266 <it22338266@my.sliit.lk>
2024-10-07 22:13:04 +0530

    Refactor index.jsx to use Firebase authentication for user login

--- ww-frontend/app/index.jsx
+++ ww-frontend/app/index.jsx
@@ -1,9 +1,21 @@
-import { View, Text } from "react-native";
-import React from "react";
 import { Redirect } from "expo-router";
+import { auth } from "../firebaseConfig";
+import React, { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
 
-const index = () => {
-  return <Redirect href={"/login"} />;
-};
+export default function App() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState(null);
 
-export default index;
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setInitializing(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  if (initializing) return null;
+
+  return user ? <Redirect href="/home" /> : <Redirect href="/login" />;
+}
