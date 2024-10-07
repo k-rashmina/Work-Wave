f2b9155fe7c14dfdfcf7bee29e52be370a79436d
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-08 00:17:27 +0530

    comment routes

--- ww-backend/src/routes/userRoutes.js
+++ ww-backend/src/routes/userRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/chamath/userController");
 
-router.post("/create", userController.createUser);
-router.get("/get/:email", userController.getUser);
-router.put("/update/:email", userController.updateUser);
-router.delete("/delete/:email", userController.deleteUser);
-router.get("/getAll", userController.getAllUsers);
+// router.post("/create", userController.createUser);
+// router.get("/get/:email", userController.getUser);
+// router.put("/update/:email", userController.updateUser);
+// router.delete("/delete/:email", userController.deleteUser);
+// router.get("/getAll", userController.getAllUsers);
 
 module.exports = router;
