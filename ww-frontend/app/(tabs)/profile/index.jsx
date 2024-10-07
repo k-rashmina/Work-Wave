660147019a91e82e90b7b0bee5cd578cbe385e95
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-08 03:46:59 +0530

    Refactor profile logout functionality

--- ww-frontend/app/(tabs)/profile/index.jsx
+++ ww-frontend/app/(tabs)/profile/index.jsx
@@ -1,14 +1,22 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
-import { logoutUser } from "../../(auth)/Auth";
 import { useRouter } from "expo-router";
+import { signOut } from "firebase/auth";
+import { auth } from "../../../firebaseConfig";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Profile = () => {
   const router = useRouter();
 
-  const handleLogout = () => {
-    logoutUser();
-    router.replace({ pathname: "/login" });
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      await AsyncStorage.removeItem("userToken")
+        .then(router.push("/login"))
+        .then(console.log("logged out"));
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   return (
