baab09b8f271eb56bfee00e2a3d7f058fce16f0f
IT22338266 <it22338266@my.sliit.lk>
2024-10-08 10:34:44 +0530

    Refactor profile/_layout.jsx to add header tint color for Profile and User Details screens

--- ww-frontend/app/(tabs)/profile/_layout.jsx
+++ ww-frontend/app/(tabs)/profile/_layout.jsx
@@ -5,7 +5,8 @@ import { Stack } from 'expo-router'
 const ProfileLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name='index' options={{headerShown: true, title: 'Profile', headerTitleAlign: 'center'}} />
+      <Stack.Screen name='index' options={{headerShown: true, title: 'Profile', headerTitleAlign: 'center', headerTintColor:'#3498DB'}} />
+      <Stack.Screen name= 'userDetails' options={{headerShown:true,title:'User Details',headerTitleAlign:'center', headerTintColor:'#3498DB'}}/>
     </Stack>
   )
 }
