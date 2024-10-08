5e956d8469a47022a26533b1ea2f939d92c065e4
asirialwis <asirichathurika900@gmail.com>
2024-10-08 16:01:01 +0530

    days controller routes

--- ww-backend/src/routes/sheduleRoutes.js
+++ ww-backend/src/routes/sheduleRoutes.js
@@ -4,10 +4,12 @@ const router = express.Router();
 
 const sheduleController = require('../controllers/asiri/assignedWorker');
 const reSheduleController = require('../controllers/asiri/resheduleController');
+const availabilityUpdate = require('../controllers/asiri/availabilityUpdate');
 
 
 router.post('/assign-worker/:workerId', sheduleController.assignWorkerWithNextAvailableDate);
 router.put('/reschedule-worker', reSheduleController.rescheduleWorkerAssignment);
+router.put('/update-available-days/:userId', availabilityUpdate.updateAvailableDays);
 
 
 module.exports = router;
