43a353e102292b2427fb55d25e19520c6f55e17e
asirialwis <asirichathurika900@gmail.com>
2024-10-08 11:57:31 +0530

    Refactor sheduleRoutes.js to add endpoint for rescheduling a worker's assignment

--- ww-backend/src/routes/sheduleRoutes.js
+++ ww-backend/src/routes/sheduleRoutes.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 
 const sheduleController = require('../controllers/asiri/assignedWorker');
+const reSheduleController = require('../controllers/asiri/resheduleController');
 
 
 router.post('/assign-worker/:workerId', sheduleController.assignWorkerWithNextAvailableDate);
+router.put('/reschedule-worker', reSheduleController.rescheduleWorkerAssignment);
+
 
 module.exports = router;
