edb65922014ca03420bbfe7719009aa39fad1bb9
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-10 03:08:24 +0530

    Refactor jobsRoute.js to add new routes for retrieving jobs for service providers

--- ww-backend/src/routes/jobsRoute.js
+++ ww-backend/src/routes/jobsRoute.js
@@ -2,7 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const createJobController = require("../controllers/kalindu/jobsController");
+const jobController = require("../controllers/yohan/jobController");
 
 router.post("/createjob", (req, res) => createJobController(req, res));
+
+router.get("/jsp/:email", (req, res) =>
+  jobController.getJobsForServiceProvider(req, res)
+);
+
+router.get("/jsp/pending/:email", (req, res) =>
+  jobController.getPendingJobsForServiceProvider(req, res)
+);
 
 module.exports = router;
