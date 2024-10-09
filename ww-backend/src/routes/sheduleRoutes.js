1b10fafeb1a822c30be4a5368a7a5c2b7d6e9a53
asirialwis <asirichathurika900@gmail.com>
2024-10-09 19:09:53 +0530

    sheduling backend files modify

--- ww-backend/src/routes/sheduleRoutes.js
+++ ww-backend/src/routes/sheduleRoutes.js
@@ -6,11 +6,13 @@ const sheduleController = require('../controllers/asiri/assignedWorker');
 const reSheduleController = require('../controllers/asiri/resheduleController');
 const availabilityUpdate = require('../controllers/asiri/availabilityUpdate');
 const availabilityGet = require('../controllers/asiri/getAvailability');
+const getsheduledDates = require('../controllers/asiri/getSheduledData');
 
 router.post('/assign-worker/:workerId', sheduleController.assignWorkerWithNextAvailableDate);
 router.put('/reschedule-worker', reSheduleController.rescheduleWorkerAssignment);
 router.put('/update-available-days/:email', availabilityUpdate.updateAvailableDays);
 router.get('/get-available-days/:email', availabilityGet.getUserAvailability);
+router.get('/get-scheduled-dates/:email', getsheduledDates.getAssignedDates);
 
 
 
