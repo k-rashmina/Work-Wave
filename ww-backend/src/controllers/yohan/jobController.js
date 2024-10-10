b2da4f642ed7b40da5f6368a2354dbdf99601142
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-10 15:30:52 +0530

    Refactor jobController.js to add new methods for retrieving accepted jobs for service providers and updating bids for jobs

--- ww-backend/src/controllers/yohan/jobController.js
+++ ww-backend/src/controllers/yohan/jobController.js
@@ -1,5 +1,6 @@
 const jobService = require("../../services/yohan/jobService");
 
+// Create a new job
 exports.createJob = async (req, res) => {
   try {
     const newJob = await jobService.createJob(req.body);
@@ -10,6 +11,18 @@ exports.createJob = async (req, res) => {
   }
 };
 
+// Get all jobs for a specific job owner
+exports.getJobsForJobOwner = async (req, res) => {
+  try {
+    const jobs = await jobService.getJobsForJobOwner(req.params.email);
+    res.status(200).json(jobs);
+  } catch (error) {
+    console.log("Error getting jobs for job owner: ", error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get all pending jobs for a specific service provider
 exports.getPendingJobsForServiceProvider = async (req, res) => {
   try {
     const jobs = await jobService.getPendingJobsForServiceProvider(
@@ -25,31 +38,7 @@ exports.getPendingJobsForServiceProvider = async (req, res) => {
   }
 };
 
-exports.getJobsForServiceProvider = async (req, res) => {
-  try {
-    const jobs = await jobService.getJobsForServiceProvider(req.params.email);
-    res.status(200).json(jobs);
-  } catch (error) {
-    console.log("Error getting jobs for service provider: ", error.message);
-    res.status(500).json({ message: error.message });
-  }
-};
-
-exports.getAcceptedJobsForServiceProvider = async (req, res) => {
-  try {
-    const jobs = await jobService.getAcceptedJobsForServiceProvider(
-      req.params.email
-    );
-    res.status(200).json(jobs);
-  } catch (error) {
-    console.log(
-      "Error getting accepted jobs for service provider: ",
-      error.message
-    );
-    res.status(500).json({ message: error.message });
-  }
-};
-
+// Update bidder's bid amount, bid description
 exports.updateBidForJob = async (req, res) => {
   try {
     const updatedJob = await jobService.updateBidForJob(
@@ -61,6 +50,33 @@ exports.updateBidForJob = async (req, res) => {
     res.status(200).json(updatedJob);
   } catch (error) {
     console.log("Error updating bid for job: ", error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get all jobs for a specific service serviceProvider
+exports.getJobsForServiceProvider = async (req, res) => {
+  try {
+    const jobs = await jobService.getJobsForServiceProvider(req.params.email);
+    res.status(200).json(jobs);
+  } catch (error) {
+    console.log("Error getting jobs for service provider: ", error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get all accepted jobs for a specific service provider
+exports.getAcceptedJobsForServiceProvider = async (req, res) => {
+  try {
+    const jobs = await jobService.getAcceptedJobsForServiceProvider(
+      req.params.email
+    );
+    res.status(200).json(jobs);
+  } catch (error) {
+    console.log(
+      "Error getting accepted jobs for service provider: ",
+      error.message
+    );
     res.status(500).json({ message: error.message });
   }
 };
