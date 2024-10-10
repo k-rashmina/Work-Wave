8f31107ef121ab65c726602b2ccce59e7a8d692d
asirialwis <asirichathurika900@gmail.com>
2024-10-10 13:08:11 +0530

    Refactor import statements for Jobs model and add sorted scheduled data route

--- ww-backend/src/routes/sheduleRoutes.js
+++ ww-backend/src/routes/sheduleRoutes.js
@@ -7,12 +7,14 @@ const reSheduleController = require('../controllers/asiri/resheduleController');
 const availabilityUpdate = require('../controllers/asiri/availabilityUpdate');
 const availabilityGet = require('../controllers/asiri/getAvailability');
 const getsheduledDates = require('../controllers/asiri/getSheduledData');
+const getsortedData = require('../controllers/asiri/sorterdSheduledData');
 
 router.post('/assign-worker/:workerId', sheduleController.assignWorkerWithNextAvailableDate);
 router.put('/reschedule-worker', reSheduleController.rescheduleWorkerAssignment);
 router.put('/update-available-days/:email', availabilityUpdate.updateAvailableDays);
 router.get('/get-available-days/:email', availabilityGet.getUserAvailability);
 router.get('/get-scheduled-dates/:email', getsheduledDates.getAssignedDates);
+router.get('/get-sorted-data/:email',getsortedData.getSortedData);
 
 
 
