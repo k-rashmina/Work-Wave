d4648bfc25bec3277f01c4aee946388c44c02dc8
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-10 15:31:03 +0530

    Refactor jobsRoute.js to add routes for retrieving jobs for a specific job owner and updating bids for jobs

--- ww-backend/src/routes/jobsRoute.js
+++ ww-backend/src/routes/jobsRoute.js
@@ -6,20 +6,24 @@ const jobController = require("../controllers/yohan/jobController");
 
 router.post("/createjob", (req, res) => createJobController(req, res));
 
-router.get("/jsp/:email", (req, res) =>
-  jobController.getJobsForServiceProvider(req, res)
+router.get("/jo/:email", (req, res) =>
+  jobController.getJobsForJobOwner(req, res)
 );
 
 router.get("/jsp/pending/:email", (req, res) =>
   jobController.getPendingJobsForServiceProvider(req, res)
 );
 
-router.get("/jsp/accepted/:email", (req, res) =>
-  jobController.getAcceptedJobsForServiceProvider(req, res)
-);
-
 router.put("/jsp/:email", (req, res) =>
   jobController.updateBidForJob(req, res)
+);
+
+router.get("/jsp/:email", (req, res) =>
+  jobController.getJobsForServiceProvider(req, res)
+);
+
+router.get("/jsp/accepted/:email", (req, res) =>
+  jobController.getAcceptedJobsForServiceProvider(req, res)
 );
 
 module.exports = router;
